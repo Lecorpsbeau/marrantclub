29e8abf1a7e1fde8701af855c3624b1ae9833243
Lecorpsbeau <helicoptodecombo@gmail.com>
2026-05-07 00:47:52 +0200

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function handler(req, res) {
-    return res.status(200).json({ 
-        message: "COUCOU ! Le serveur est bien à jour !",
-        time: new Date().toLocaleTimeString('fr-FR')
-    });
-};
